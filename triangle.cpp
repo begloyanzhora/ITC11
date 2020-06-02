1544395eff9852339317fca4dd6ba20a14d98f32
Zhora <begloyan.jora@gmail.com>
2020-06-02 22:49:54 +0400

    Added triangle.cpp and stringReverse.cpp files.

---
+++ triangle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main () {
+    int n;
+
+    std::cout << "Enter n: ";
+    std::cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j < i; j++) {
+            std::cout << " ";
+        }
+
+        for (int j = 1; j <= (n * 2 - (2 * i -1)); j++) {
+            if (i == 1 || j == 1 || j == (n * 2 - (2 * i - 1) )) {
+                std::cout << "*";
+            } else {
+                std::cout << " ";
+            }
+        }
+
+        std::cout << "\n";
+    }
+
+    return 0;
+}
