1880c63f9dde84a9cb2ab261547490915a0a6883
Zhora Begloyan <begloyan.jora@gmail.com>
2020-07-11 23:01:42 +0400

    Added count possible ways function.

---
+++ Algorithms/count_possible_ways/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> mem(1, 0);
+
+int fib (int n) {
+	if (n < mem.size()) {
+		return mem[n];
+	}
+
+	if (n < 2) {
+		mem.push_back(n);
+		return mem[n];
+	}
+
+	mem.push_back(fib(n - 1) + fib(n - 2));
+	return mem[n];
+
+}
+
+int countWays (int n) {
+	return fib(n + 2) * fib(n + 2);
+}
+
+int main() {
+
+	int n = 3;
+
+	std::cout << countWays(n) << std::endl;
+
+	return 0;
+}
