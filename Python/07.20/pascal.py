b196d16b80104a86fee8629ac0c9aa2d1e554a3f
Zhora Begloyan <begloyan.jora@gmail.com>
2020-07-20 21:18:02 +0400

    Added homework files for 07.20.

---
+++ Python/07.20/pascal.py
@@ -0,0 +1,24 @@
+def getValue (r, c):
+	if c == 1 or c == r:
+		return 1
+
+	return getValue(r - 1, c - 1) + getValue(r - 1, c)
+
+def pascalTriangle (n):
+	for r in range(1, n + 1):
+
+		for c in range(n - r):
+			print(' ', end = '')
+
+		for c in range(1, r + 1):
+			print(getValue(r, c), end=' ')
+
+		print()
+
+def main ():
+	print("How many rows of Pascal's triangle do you want to print?", end = ' ')
+	n = int(input())
+	pascalTriangle(n)
+
+if __name__ == "__main__":
+	main()
